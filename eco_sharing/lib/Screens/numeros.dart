8f9991f91a0cac593b3fa89f47fdc09175e545cb
Mikel-Jorge <mjorgesote@educacion.navarra.es>
2023-01-23 10:53:51 +0100

    Ejercicio numeros en filas y columnas

---
+++ eco_sharing/lib/Screens/numeros.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Numeros extends StatelessWidget {
+  const Numeros({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Numeros'),
+        backgroundColor: Colors.amber,
+        elevation: 20,
+        shadowColor: Colors.black,
+      ),
+      body: Column(children: [
+        Row(children: [
+          creaBloque(color: Colors.lightBlue, text: '1'),
+          creaBloque(color: Colors.lightGreen, text: '2'),
+          creaBloque(color: Colors.red, text: '3')
+        ]),
+        Row(children: [
+          creaBloque(color: Colors.red, text: '4'),
+          creaBloque(color: Colors.lightBlue, text: '5'),
+          creaBloque(color: Colors.lightGreen, text: '6')
+        ]),
+        Row(children: [
+          creaBloque(color: Colors.lightGreen, text: '7'),
+          creaBloque(color: Colors.red, text: '8'),
+          creaBloque(color: Colors.lightBlue, text: '9')
+        ]),
+      ]),
+    );
+  }
+
+  Expanded creaBloque({required Color color, required String text}) {
+    return Expanded(
+        child: Container(
+            color: color,
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              text,
+              textAlign: TextAlign.center,
+            )));
+  }
+}
--- eco_sharing/lib/main.dart
+++ eco_sharing/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:eco_sharing/Screens/counter_screen.dart';
+// import 'package:eco_sharing/Screens/counter_screen.dart';
+import 'package:eco_sharing/Screens/numeros.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class EcoSharing extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterScreen(),
+      home: Numeros(),
     );
   }
 }
