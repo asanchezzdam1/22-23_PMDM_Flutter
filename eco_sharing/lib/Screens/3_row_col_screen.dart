5479a43cb91d71c9095be891972a5304d3d0e3b4
Mikel-Jorge <mjorgesote@educacion.navarra.es>
2023-01-25 09:02:55 +0100

    Filas expandidas

--- eco_sharing/lib/Screens/3_row_col_screen.dart
+++ eco_sharing/lib/Screens/3_row_col_screen.dart
@@ -13,21 +13,27 @@ class RowColScreen extends StatelessWidget {
         shadowColor: Colors.black,
       ),
       body: Column(children: [
-        Row(children: [
-          creaBloque(color: Colors.lightBlue, text: '1'),
-          creaBloque(color: Colors.lightGreen, text: '2'),
-          creaBloque(color: Colors.red, text: '3')
-        ]),
-        Row(children: [
-          creaBloque(color: Colors.red, text: '4'),
-          creaBloque(color: Colors.lightBlue, text: '5'),
-          creaBloque(color: Colors.lightGreen, text: '6')
-        ]),
-        Row(children: [
-          creaBloque(color: Colors.lightGreen, text: '7'),
-          creaBloque(color: Colors.red, text: '8'),
-          creaBloque(color: Colors.lightBlue, text: '9')
-        ]),
+        Expanded(
+          child: Row(children: [
+            creaBloque(color: Colors.lightBlue, text: '1'),
+            creaBloque(color: Colors.lightGreen, text: '2'),
+            creaBloque(color: Colors.red, text: '3')
+          ]),
+        ),
+        Expanded(
+          child: Row(children: [
+            creaBloque(color: Colors.red, text: '4'),
+            creaBloque(color: Colors.lightBlue, text: '5'),
+            creaBloque(color: Colors.lightGreen, text: '6')
+          ]),
+        ),
+        Expanded(
+          child: Row(children: [
+            creaBloque(color: Colors.lightGreen, text: '7'),
+            creaBloque(color: Colors.red, text: '8'),
+            creaBloque(color: Colors.lightBlue, text: '9')
+          ]),
+        ),
       ]),
     );
   }
@@ -36,10 +42,11 @@ class RowColScreen extends StatelessWidget {
     return Expanded(
         child: Container(
             color: color,
-            padding: const EdgeInsets.all(50),
-            child: Text(
-              text,
-              textAlign: TextAlign.center,
+            child: Center(
+              child: Text(
+                text,
+                textAlign: TextAlign.center,
+              ),
             )));
   }
 }
